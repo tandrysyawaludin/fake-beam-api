aa337c9a3a53c28a525bb2386c525b6025c196d1
tandrysyawaludin <syawaludintandry@gmail.com>
2022-01-05 00:22:26 +0700

    update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const vehiclesJson = require('./data/vehicles.json');
 const tripsJson = require('./data/trips.json');
 
